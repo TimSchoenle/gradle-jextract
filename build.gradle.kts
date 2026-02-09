c124bdc1d41d9494419b0fc10df050f5c34335a6
Tim Schönle <13711365+TimSchoenle@users.noreply.github.com>
2026-02-09 16:38:56 +0100

    build: setup maven central publish (#13)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import com.vanniktech.maven.publish.DeploymentValidation
+import com.vanniktech.maven.publish.GradlePlugin
+import com.vanniktech.maven.publish.JavadocJar
+
 plugins {
     `java-gradle-plugin`
     `maven-publish`
@@ -77,7 +81,7 @@ gradlePlugin {
         create("jextract") {
             id = gradlePluginId
             implementationClass = "de.timscho.jextract.JextractPlugin"
-            displayName = "Modern Jextract Plugin"
+            displayName = "Jextract Gradle Plugin"
             description = "Downloads and runs jextract to generate Java FFM bindings"
         }
     }
@@ -86,25 +90,36 @@ gradlePlugin {
 }
 
 mavenPublishing {
-    coordinates(group.toString(), "de.timscho.jextract", version.toString())
+    coordinates(group.toString(), "gradle-jextract", version.toString())
+
+    configure(GradlePlugin(javadocJar = JavadocJar.Javadoc()))
 
     pom {
+        name.set("Jextract Gradle Plugin")
+        description.set("A Gradle plugin that automates the download and execution of jextract to generate Java Foreign Function & Memory (FFM) API bindings from C header files with bundled library loading support.")
+        url.set("https://github.com/TimSchoenle/gradle-jextract")
         inceptionYear.set("2026")
+
         licenses {
             license {
                 name.set("The Apache License, Version 2.0")
                 url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("repo")
             }
         }
         developers {
             developer {
                 id.set("TimSchoenle")
                 name.set("Tim Schönle")
+                url.set("https://github.com/TimSchoenle")
             }
+        }
+        scm {
+            url.set("https://github.com/TimSchoenle/gradle-jextract")
         }
     }
 
-    publishToMavenCentral(automaticRelease = false)
+    publishToMavenCentral(automaticRelease = true, validateDeployment = DeploymentValidation.PUBLISHED)
 
     // We only want to sign publications when running on CI
     if (providers.environmentVariable("CI").isPresent) {
